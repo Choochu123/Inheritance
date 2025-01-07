e3a10be59b3a182cdbed33d71e2ccdc16b2bb1dd
y.chu4 <jonathan.chu.yl@gmail.com>
2025-01-07 11:40:05 -0700

    start 2D Arrays

---
+++ src/TwoD_Arrays/Ex1_Golf.java
@@ -0,0 +1,12 @@
+package TwoD_Arrays;
+
+public class Ex1_Golf {
+
+    public static void run() {
+        String[] names = {"Joe", "Serena", " Gill", "Stu"};
+
+        int[][] scorecard = new int[4][18];
+
+
+    }//run
+}

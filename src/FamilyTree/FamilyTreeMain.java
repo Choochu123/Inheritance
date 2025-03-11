5f55f4c50dc9b255c98aca54d752ba9d4564c086
y.chu4 <jonathan.chu.yl@gmail.com>
2025-03-11 11:30:15 -0600

    Added names to Family Tree

--- src/FamilyTree/FamilyTreeMain.java
+++ src/FamilyTree/FamilyTreeMain.java
@@ -14,15 +14,31 @@ public class FamilyTreeMain {
         person1.addChild(new Person("Bobette", "Zimbabwe"));
 
 //Gen 3
+        //Susan
         person1.children.get(0).addChild(new Person("Zain", "Zimbabwe"));/* adding to Susan */
+        //Bobette
         person1.children.get(2).addChild(new Person("Bob III", "Belise"));
         person1.children.get(2).addChild(new Person("Jasmine", "Canada"));
+        //Jill
+        person1.children.get(1).addChild(new Person("Jonathan", "Canada"));
+        person1.children.get(1).addChild(new Person("Zev", "Canada"));
+
 //Gen 4
+        //Zain
         person1.children.get(0).children.get(0).addChild(new Person("Zain Jr", "Mexico"));
         person1.children.get(0).children.get(0).addChild(new Person("Zainette", "Mexico"));
+        //BobIII
         person1.children.get(2).children.get(0).addChild(new Person("Bob IV", "Belise"));
+        //Jasmine
         person1.children.get(2).children.get(1).addChild(new Person("Zaiesha", "Canada"));
         person1.children.get(2).children.get(1).addChild(new Person("Steve", "USA"));
+        //Jonathan
+        person1.children.get(1).children.get(0).addChild(new Person("Elliott", "Spain"));
+        //Zev
+        person1.children.get(1).children.get(1).addChild(new Person("Sorata", "Japan"));
+
+//Gen 5
+        person1.children.get(1).children.get(1).children.get(0).addChild(new Person("Aaron", "North Korea"));
 
     }//run
 
@@ -35,6 +51,10 @@ public class FamilyTreeMain {
     public static void printCanadian(Person ptemp) {
 
     }//printCanadian
+
+    public static void printFamily(Person ptemp){
+
+    }
 
 
 }//FamilyTreeMain
